3bc71ee377b9bcdf03295cc751d04fee06f37124
erick <erickmm@al.insper.edu.br>
2024-10-23 19:57:56 -0300

    parte 2

--- funcoes.py
+++ funcoes.py
@@ -9,4 +9,11 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
 
     return posicoes 
     
-
+def preenche_frota(frota,nome_nav,linha, coluna, orientacao, tamanho):
+    nova_posi=define_posicoes(linha,coluna,orientacao,tamanho)
+    if nome_nav in frota:
+        frota[nome_nav].append(nova_posi)
+    else:
+        frota[nome_nav]=[nova_posi]
+    return frota
+    
