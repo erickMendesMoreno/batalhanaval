16929d6158af136ddd6c556ebca3b4eda62b1453
fogaca <joaopedrofgc1@gmail.com>
2024-10-27 21:11:40 -0300

    parte 7

--- programa.py
+++ programa.py
@@ -11,22 +11,29 @@ frota = {
     "contratorpedeiro":[],
     "submarino": [],
 }
+quantidade = {
+    "porta-aviões":1,
+    "navio-tanque":2,
+    "contratorpedeiro":3,
+    "submarino": 4,
+}
 
 orientacoes = [ "vertical","horizontal"]
 for nome, posicao in frota.items():
-    valida = False
-    while not valida:
-        print(f"Insira as informações referentes ao navio {nome} que possui tamanho {espaco[nome]}")
-        linha = int(input("Linha: "))
-        coluna = int(input("Coluna: "))
-        if espaco[nome] > 1 :
-            orientacao = orientacoes[int(input("[1] Vertical [2] Horizontal >"))-1]
-        else:
-            orientacao = orientacoes[0] #verical
-        pos = [linha,coluna]
-        valida = posicao_valida(frota,linha,coluna,orientacao,espaco[nome])
-        if not valida:
-            print("Esta posição não está válida!")
-    frota = preenche_frota(frota,nome,linha,coluna,orientacao,espaco[nome])
+    for i in range(quantidade[nome]):
+        valida = False
+        while not valida:
+            print(f"Insira as informações referentes ao navio {nome} que possui tamanho {espaco[nome]}")
+            linha = int(input("Linha: "))
+            coluna = int(input("Coluna: "))
+            if espaco[nome] > 1 :
+                orientacao = orientacoes[int(input("[1] Vertical [2] Horizontal >"))-1]
+            else:
+                orientacao = orientacoes[0] #verical
+            pos = [linha,coluna]
+            valida = posicao_valida(frota,linha,coluna,orientacao,espaco[nome])
+            if not valida:
+                print("Esta posição não está válida!")
+        frota = preenche_frota(frota,nome,linha,coluna,orientacao,espaco[nome])
     
 print(frota)
